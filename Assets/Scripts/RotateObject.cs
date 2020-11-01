71586a23b213cdbf5d2632b542bdc19d5c448709
Quickz <alexserg@inbox.lv>
2020-11-01 21:07:24 +0200

    added the project

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotation = new Vector3(0f, 1f, 0f);
+
+    [SerializeField]
+    private float speed = 25f;
+
+    private void Update()
+    {
+        transform.RotateAround(
+            transform.position,
+            rotation,
+            speed * Time.deltaTime);
+    }
+}
